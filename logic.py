fc73e29cbcd5842a7f50f212b50fb3063a74d0e5
themysterysolver <arjunprabhakar1910@gmail.com>
2025-11-26 14:37:04 +0530

    FEAT:SJF

--- logic.py
+++ logic.py
@@ -16,7 +16,7 @@ def algoSolver(at,bt,option,tq=None,p=None):
     if option == "First come first served(FCFS)":
         ans = FCFS(at,bt)
     elif option == "Shortest job first(SJF)":
-        pass
+        ans = SJF(at,bt)
     elif option == "Shortest remaining time first(SRTF)":
         pass
     elif option == "Premptive Priority scheduling":
@@ -45,5 +45,39 @@ def FCFS(at,bt):
         tat[i] = ct[i]-at[i]
         wt[i] = tat[i]-bt[i]
 
-    return {"Process":[chr(ord('A')+i) for i in range(n)],"Arrival Time":at,"Burst TimeT":bt,"Completion Time":ct,"Turn Around Time":tat,"Waiting Time":wt}    
+    return {"Process":[chr(ord('A')+i) for i in range(n)],"Arrival Time":at,"Burst Time":bt,"Completion Time":ct,"Turn Around Time":tat,"Waiting Time":wt}
+
+def SJF(at,bt):
+    n = len(at)
+    ct = [0]*n
+    tat = [0]*n
+    wt = [0]*n
+    
+    completed = [False]*n
+    done = 0
+    time = 0
+
+    process = sorted(range(n),key=lambda i:at[i])
+
+    while done<n:
+        idx = -1
+        minbt = float('inf')
+        for i in process:
+            if not completed[i] and at[i]<=time:
+                if bt[i]<minbt:
+                    idx = i
+                    minbt = bt[i]
+
+        if idx==-1:
+            time+=1
+        else:
+            time+=bt[idx]
+            ct[idx] = time
+            completed[idx] = True
+            done+=1
+            tat[idx] = ct[idx]-at[idx]
+            wt[idx] = tat[idx]-bt[idx]
+
+
+    return {"Process":[chr(ord('A')+i) for i in range(n)],"Arrival Time":at,"Burst Time":bt,"Completion Time":ct,"Turn Around Time":tat,"Waiting Time":wt}        
     
